927055f6e0849b946ffbfcd8ad435ec0ce937736
vsetia <setia.v@northeastern.edu>
2023-06-12 17:33:44 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js"
+import cors from 'cors'
+import AuthController from "./users/auth-controller.js";
+import session from "express-session";
+const app = express()
+app.use(
+    session({
+      secret: "any string",
+      resave: false,
+      saveUninitialized: true,
+    })
+   );
+   
+app.use(express.json());
+app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+   );
+AuthController(app);
+HelloController(app)
+UserController(app)
+TuitsController(app);
+app.listen(process.env.PORT || 4000);
---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,60 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const createTuit = (req, res) => {
+
+    const newTuit = req.body;
+  newTuit._id = (new Date()).getTime()+'';
+  newTuit.likes = 0;
+  newTuit.replies = 0;
+  newTuit.retuits = 0;
+  newTuit.dislikes = 0;
+  newTuit.liked = false;
+  newTuit.handle = "@boeing";
+  newTuit.time = "2h";
+  newTuit.image="spacex.webp"
+  
+  tuits.unshift(newTuit);
+  res.json(newTuit);
+//   console.log(tuits);
+
+}
+const findTuits  = (req, res) => {
+    const topic = req.query.topic
+    if(topic) {
+      const tuitsOfType = tuits
+        .filter(u => u.topic === topic)
+      res.json(tuitsOfType)
+      return
+    }
+    res.json(tuits)
+}
+const updateTuit = (req, res) => {
+    const tuitdId = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex((t) => t._id === tuitdId)
+    tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+
+}
+const deleteTuit = (req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    tuits = tuits.filter((t) =>
+      t._id !== tuitdIdToDelete);
+    res.sendStatus(200);
+}
+
+export default (app) => {
+ app.post('/api/tuits', createTuit);
+ app.get('/api/tuits', findTuits);
+ app.put('/api/tuits/:tid', updateTuit);
+ app.delete('/api/tuits/:tid', deleteTuit);
+}
+
+
+
+// implement this
+// implement this
+// implement this
+// implement this
+
